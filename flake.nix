cd07c9167669ad599caeb0681e9c22ab9204580e
Jay Looney <jay.m.looney@gmail.com>
2024-12-27 14:34:28 -0600

    Override intel driver with modesetting so I can actually build and switch configs again

--- flake.nix
+++ flake.nix
@@ -14,6 +14,8 @@
           # https://github.com/NixOS/nixos-hardware/blob/master/README.md#using-nix-flakes-support
           nixos-hardware.nixosModules.gpd-pocket-3
           ./configuration.nix
+           # override from nixos-hardware
+          ({config, lib, ...}: { services.xserver.videoDrivers = lib.mkForce [ "modesetting" ]; })
         ];
       };
     }; 
