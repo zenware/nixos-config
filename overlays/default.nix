836a31c11901a1c49b2c99803449ebb23b867d8e
Jay Looney <jay.m.looney@gmail.com>
2025-11-06 00:37:26 -0600

    feat: Add CMake3 overlays to keep game emulation software working.

---
+++ overlays/default.nix
@@ -0,0 +1,40 @@
+{ nixpkgs }:
+  let
+    fixCmake = pkg: pkg.overrideAttrs (old: {
+      cmakeFlakes = (old.cmakeFlags or []) ++ [
+        (nixpkgs.lib.cmakeFeature "CMAKE_POLICY_VERSION_MINIMUM" "3.10")
+      ];
+    });
+    cmake3Overlay = final: prev:
+      nixpkgs.lib.mapAttrs (
+        n: pkg:
+        pkg.overrideAttrs (old: {
+          cmakeFlags = old.cmakeFlags or [ ] ++ [ (nixpkgs.lib.cmakeFeature "CMAKE_POLICY_VERSION_MINIMUM" "3.10") ];
+        })
+      ) {
+          inherit (prev) hpipm;
+      };
+    libretroCmake3Overlay = final: prev: {
+      libretro = prev.libretro // {
+        thepowdertoy = prev.libretro.thepowdertoy.overrideAttrs(old: {
+          cmakeFlags = old.cmakeFlags or [ ] ++ [ (nixpkgs.lib.cmakeFeature "CMAKE_POLICY_VERSION_MINIMUM" "3.10") ];
+        });
+
+        tic80 = prev.libretro.tic80.overrideAttrs(old: {
+          cmakeFlags = old.cmakeFlags or [ ] ++ [ (nixpkgs.lib.cmakeFeature "CMAKE_POLICY_VERSION_MINIMUM" "3.10") ];
+        });
+
+        citra = prev.libretro.citra.overrideAttrs(old: {
+          cmakeFlags = old.cmakeFlags or [ ] ++ [ (nixpkgs.lib.cmakeFeature "CMAKE_POLICY_VERSION_MINIMUM" "3.10") ];
+        });
+
+        dolphin = prev.libretro.dolphin.overrideAttrs(old: {
+          cmakeFlags = old.cmakeFlags or [ ] ++ [ (nixpkgs.lib.cmakeFeature "CMAKE_POLICY_VERSION_MINIMUM" "3.10") ];
+        });
+      };
+    };
+in
+[
+  cmake3Overlay
+  libretroCmake3Overlay
+]
