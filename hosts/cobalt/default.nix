b8cd94925e62e915ed9ffc52a2bd26d1412181b5
Jay Looney <jay.m.looney@gmail.com>
2025-11-25 10:32:02 -0600

    cobalt snapshot

--- hosts/cobalt/default.nix
+++ hosts/cobalt/default.nix
@@ -5,7 +5,13 @@
     ../../modules/nixos/audio.nix
     #../../modules/nixos/desktop.nix
     # https://github.com/NixOS/nixos-hardware/blob/master/README.md#using-nix-flakes-support
+    # TODO: This module doesn't exist yet.
+    #inputs.nixos-hardware.nixosModules.asus-zenbook-ux390u
+    /home/jml/Workspace/nixos-hardware/asus/zenbook/ux390ua
     ./hardware-configuration.nix
     ./configuration.nix
+    ../../modules/nixos/gaming.nix
   ];
+
+  zw.gaming.enable = true;
 }
