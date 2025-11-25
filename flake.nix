e3e28fa662ecf19f3e8f5013334045fc2c6f7496
Jay Looney <jay.m.looney@gmail.com>
2025-11-25 15:40:37 -0600

    horribly cobble together the ability to use both home-manager switch and nixos-rebuild switch against the same flake

--- flake.nix
+++ flake.nix
@@ -13,73 +13,96 @@
     disko.inputs.nixpkgs.follows = "nixpkgs";
     stylix.url = "github:nix-community/stylix";
     stylix.inputs.nixpkgs.follows = "nixpkgs";
+
+    #obsidian-nvim.url = "github:epwalsh/obsidian.nvim";
+    nvf = {
+      url = "github:notashelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+      #inputs.obsidian-nvim.follows = "obsidian-nvim";
+    };
   };
   # https://nix.dev/tutorials/nix-language.html#named-attribute-set-argument
-  outputs = inputs@{self, nixpkgs, nixos-hardware, home-manager, sops-nix, lanzaboote, disko, stylix, ...}:
-  let
-    mkSystem = (import ./lib {
-      inherit nixpkgs home-manager inputs;
-    }).mkSystem;
-  in
-  {
-    lib = {
-      mkSystem = mkSystem;
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      nixos-hardware,
+      home-manager,
+      sops-nix,
+      lanzaboote,
+      disko,
+      stylix,
+      nvf,
+      ...
+    }:
+    let
+      zwLib = import ./lib {
+        inherit nixpkgs home-manager inputs;
+      };
+      mkSystem = zwLib.mkSystem;
+      mkHome = zwLib.mkHome;
+      mkHomeConfigs = zwLib.mkHomeConfigs;
+
+      # NOTE: Currently these are exclusively user-profiles which use home-manager.
+      # Their home-manager specific declarations are at ../users/${username}/home.nix
+      system = "x86_64-linux"; # TODO: Improve this from only static x86 to dynamic.
+      homeUserProfiles = {
+        jml = mkHome {
+          inherit system; # inputs;
+          username = "jml";
+          extraModules = [ nvf.homeManagerModules.default ];
+        };
+      };
+    in
+    {
+      lib = {
+        mkSystem = mkSystem;
+      };
+      # NOTE: Run `nix flake show` to see what this flake has to offer.
+      # TODO: Enable automated formatting with something like numtide/treefmt-nix
+      nixosConfigurations = {
+        neon = mkSystem {
+          hostname = "neon";
+          users = [ "jml" ];
+        };
+        lithium = mkSystem {
+          hostname = "lithium";
+          # extraModules = [ inputs.sops-nix.nixosModules.sops ];
+          users = [
+            "jml"
+            "breakglass"
+          ];
+        };
+        titanium = mkSystem {
+          hostname = "titanium";
+          users = [
+            "jml"
+          ];
+          homeUsers = {
+            jml = homeUserProfiles.jml.module;
+          };
+          #extraModules = [ (import ./overlays) ];
+          # NOTE: If I'm using a home-manager configuration on a given host,
+          # I also need to include the relevant modules.
+          # TODO: Can I instead self-reference the homeConfigurations in this flake?
+          extraModules = [
+            stylix.nixosModules.stylix
+          ];
+        };
+        # `nix build .#nixosConfigurations.installIso.config.system.build.isoImage`
+        # https://github.com/nix-community/nixos-generators
+        installIso = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+          ];
+          specialArgs = { inherit inputs; };
+        };
+      };
+
+      # For Debugging: `home-manager build --flake .` or `nix build .#homeConfigurations."jml".activationPackage`
+      # `home-manager switch --flake .#jml`
+      # https://nix-community.github.io/home-manager/options.xhtml
+      homeConfigurations = mkHomeConfigs homeUserProfiles;
     };
-    # NOTE: Run `nix flake show` to see what this flake has to offer.
-    # TODO: Enable automated formatting with something like numtide/treefmt-nix
-    nixosConfigurations = {
-      neon = mkSystem {
-        hostname = "neon";
-        users = [ "jml" ];
-      };
-      lithium = mkSystem {
-        hostname = "lithium";
-        # extraModules = [ inputs.sops-nix.nixosModules.sops ];
-        users = [
-          "jml"
-          "breakglass"
-        ];
-      };
-      titanium = mkSystem {
-        hostname = "titanium";
-        users = [
-          "jml"
-        ];
-        extraModules = [
-          #(import ./overlays)
-          #stylix.nixosModules.stylix
-          { nixpkgs.config.allowUnfree = true; }
-          lanzaboote.nixosModules.lanzaboote
-          disko.nixosModules.disko
-          ./hosts/titanium/disko.nix
-          ({ pkgs, lib, ... }: {
-            environment.systemPackages = [ pkgs.sbctl ];
-            boot.loader.systemd-boot.enable = lib.mkForce false;
-            boot.lanzaboote.enable = true;
-            boot.lanzaboote.pkiBundle = "/var/lib/sbctl";
-          })
-        ];
-      };
-      cobalt = mkSystem {
-        hostname = "cobalt";
-        users = [ "jml" ];
-      };
-      # `nix build .#nixosConfigurations.installIso.config.system.build.isoImage`
-      # https://github.com/nix-community/nixos-generators
-      installIso = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-        ];
-        specialArgs = {inherit inputs;};
-      };
-    }; 
-    homeConfigurations = {
-      "jml" = home-manager.lib.homeManagerConfiguration {
-        modules = [
-          ./users/jml/home.nix
-        ];
-      };
-    };
-  };
 }
--- lib/default.nix
+++ lib/default.nix
@@ -1,68 +1,113 @@
-{ nixpkgs, home-manager, inputs, ... }:
+{
+  nixpkgs,
+  home-manager,
+  inputs,
+  ...
+}:
 let
   allOverlays = import (../overlays) { inherit nixpkgs; };
-in
-{
-  # It's not really that I care about whether a system is a desktop system or
-  # a server system, but moreso that I care about whether a system is headless or not.
-  # I also care about things like if it's darwin, or wsl.
-  # TODO: Expand this to actually make use of extraSpecialArgs and pass special
-  # args to the relevant places.
-  mkSystem = {
-    hostname,
-    system ? "x86_64-linux",
-    users ? [],
-    extraModules ? [],
-    extraSpecialArgs ? {}
-  }:
-  let
-    pkgs_with_overlays = import nixpkgs {
+  getPkgs =
+    system:
+    import nixpkgs {
       inherit system;
       overlays = allOverlays;
     };
-    hostModule = import ../hosts/${hostname} {
-      inherit inputs;
-      pkgs = pkgs_with_overlays;
-    };
-    userModules = map (name:
-      import ../users/${name} {
-        pkgs = pkgs_with_overlays;
-        lib = nixpkgs.lib;
-      }
-    ) users;
-
-    homeUserNames = builtins.filter (name:
-      builtins.pathExists ../users/${name}/home.nix
-    ) users;
-
-    homeUsers = nixpkgs.lib.listToAttrs (map (name: {
-      name = name;
-      value = import ../users/${name}/home.nix {
-        username = name;
-        pkgs = pkgs_with_overlays;
-        lib = nixpkgs.lib;
+in
+{
+  mkSystem =
+    {
+      hostname,
+      system ? "x86_64-linux",
+      users ? [ ],
+      extraModules ? [ ],
+      homeUsers ? { },
+      extraSpecialArgs ? { },
+    }:
+    let
+      pkgs_with_overlays = import nixpkgs {
+        inherit system;
+        overlays = allOverlays;
       };
-    }) homeUserNames);
-  in
+      hostModule = import ../hosts/${hostname} {
+        inherit inputs;
+        pkgs = pkgs_with_overlays;
+      };
+      userModules = map (
+        name:
+        import ../users/${name} {
+          pkgs = pkgs_with_overlays;
+          lib = nixpkgs.lib;
+        }
+      ) users;
+
+      formattedHomeUsers = nixpkgs.lib.mapAttrs (username: moduleList: {
+        imports = moduleList;
+      }) homeUsers;
+    in
     nixpkgs.lib.nixosSystem {
       inherit system;
-      # pkgs = import inputs.nixpkgs {
-      #   inherit system;
-      #   overlays = allOverlays;
-      #   config = { allowUnfree = true; nvidia.acceptLicense = true; };
-      # };
-      modules = [ hostModule ]
-        ++ userModules
-        ++ extraModules
-        ++ (if homeUserNames != [] then [
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.backupFileExtension = "hm-bak";
-            home-manager.users = homeUsers;
-          }
-        ] else []);
+      modules = [
+        hostModule
+      ]
+      ++ userModules
+      ++ extraModules
+      ++ (
+        if homeUsers != { } then
+          [
+            home-manager.nixosModules.home-manager
+            {
+              #home-manager.useGlobalPkgs = true;  # NOTE: Incompatible with nixpkgs.{config,overlays}
+              home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "hm-bak";
+
+              # Directly inject the module lists? (isn't this the problem?)
+              home-manager.users = formattedHomeUsers;
+              home-manager.extraSpecialArgs = { inherit inputs; };
+            }
+          ]
+        else
+          [ ]
+      );
       specialArgs = {
         inherit inputs hostname;
-      } // extraSpecialArgs;
+      }
+      // extraSpecialArgs;
     };
+
+  getUserHomeModule =
+    username: pkgs: inputs:
+    import ../users/${username}/home.nix {
+      inherit username pkgs inputs;
+      lib = nixpkgs.lib;
+    };
+
+  /**
+    This function returns an attribute set { module, config }.
+  */
+  mkHome =
+    {
+      username,
+      system ? "x86_64-linux",
+      extraModules ? [ ],
+    }:
+    let
+      pkgs_with_overlays = getPkgs system;
+      moduleList = [
+        (import ../users/${username}/home.nix {
+          inherit inputs username;
+          pkgs = pkgs_with_overlays;
+          lib = nixpkgs.lib;
+        })
+      ]
+      ++ extraModules;
+    in
+    {
+      module = moduleList;
+      config = home-manager.lib.homeManagerConfiguration {
+        pkgs = pkgs_with_overlays;
+        modules = moduleList;
+      };
+    };
+
+  mkHomeConfigs = userProfiles: nixpkgs.lib.mapAttrs (username: profile: profile.config) userProfiles;
 }
