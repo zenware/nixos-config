5339eeebce9a546a6f7b3dc34ddb4f0eaf7da39e
Jay Looney <jay.m.looney@gmail.com>
2025-11-25 11:56:03 -0600

    feat: add gamemode and gamescope /w CAP_SYS_NICE

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -5,11 +5,12 @@
     zw.gaming.enable = lib.mkEnableOption "Enable Gaming";
   };
 
+  # https://wiki.nixos.org/wiki/Category:Gaming
   config = lib.mkIf config.zw.gaming.enable {
     environment.systemPackages = with pkgs; [
       mangohud
       protonup-qt
-      # lutris  # TODO: Having an issue after flake update
+      lutris  # TODO: Having an issue after flake update
       bottles
       heroic
     ];
@@ -19,6 +20,12 @@
       localNetworkGameTransfers.openFirewall = true;
       protontricks.enable = true;
       gamescopeSession.enable = true;
+    };
+
+    programs.gamemode.enable = true;
+    programs.gamescope = {
+      enable = true;
+      capSysNice = true;
     };
   };
 }
