a2767f8b634053f2d4aa6bb8c66d3a0be210bb6a
Jay Looney <jay.m.looney@gmail.com>
2026-09-14 15:00:14 -0500

    declare nix wallpaper

--- users/jml/home/noctalia.nix
+++ users/jml/home/noctalia.nix
@@ -1,3 +1,10 @@
+{ pkgs, ... }:
+let
+  wallpaper = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/8957e93c95867faafec7f9988cedddd6837859fa/wallpapers/nix-wallpaper-binary-black.png";
+    hash = "sha256-mhSh0wz2ntH/kri3PF5ZrFykjjdQLhmlIlDDGFQIYWw=";
+  };
+in
 {
   # Is there any way of doing home-manager separately from nixosConfig/darwinConfig
   # and yet still reference the contents of the nixosConfig to decide whether to enable
@@ -5,21 +12,28 @@
   # https://docs.noctalia.dev/getting-started/nixos/#config-ref
   programs.noctalia = {
     enable = true;
-    settings.bar.main = {
-      enabled = true;
-      density = "compact";
-      position = "right";
+    settings = {
+      wallpaper = {
+        enabled = true;
+        default.path = "${wallpaper}";
+      };
 
-      capsule = false;
-      start = [
-        "network"
-        "bluetooth"
-      ];
-      center = [ "workspaces" ];
-      end = [
-        "battery"
-        "clock"
-      ];
+      bar.main = {
+        enabled = true;
+        density = "compact";
+        position = "right";
+
+        capsule = false;
+        start = [
+          "network"
+          "bluetooth"
+        ];
+        center = [ "workspaces" ];
+        end = [
+          "battery"
+          "clock"
+        ];
+      };
     };
   };
 }
