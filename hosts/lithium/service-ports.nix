3cc4950a6129c5179955cc63d035dd67830a7209
Jay Looney <jay.m.looney@gmail.com>
2026-09-22 09:47:41 -0500

    fix: move option-default ports to config settings

--- hosts/lithium/service-ports.nix
+++ hosts/lithium/service-ports.nix
@@ -31,52 +31,13 @@ in
       options = {
         tcp = lib.mkOption {
           type = lib.types.attrsOf lib.types.port;
-          default = {
-            adguardDns = 53;
-            caddyHttp = 80;
-            caddyHttps = 443;
-            sambaNetbiosSession = 139;
-            sambaSmb = 445;
-            forgejo = 3000;
-            grafana = 3001;
-            uptimeKuma = 3002;
-            immichMachineLearning = 3003;
-            adguardHome = 3004;
-            kanidmLdap = 3636;
-            sambaWsdd = 5357;
-            miniflux = 8081;
-            nextcloudBackend = 8083;
-            jellyfinHttp = 8096;
-            jellyfinHttps = 8920;
-            homeAssistant = 8123;
-            vaultwarden = 8222;
-            syncthingGui = 8384;
-            kanidmHttps = 8443;
-            calibreWeb = 8883;
-            hermesWebhook = 8644;
-            hermesDashboard = 9119;
-            hermesWebui = 18787;
-            syncthingTransfer = 22000;
-            palworldRcon = 25575;
-            immich = 2283;
-            paperless = 28981;
-          };
+          default = { };
         };
 
         udp = lib.mkOption {
           type = lib.types.attrsOf lib.types.port;
-          default = {
-            adguardDns = 53;
-            sambaNetbiosNs = 137;
-            sambaNetbiosDgm = 138;
-            sambaWsdd = 3702;
-            jellyfinSsdp = 1900;
-            jellyfinClientDiscovery = 7359;
-            palworldGame = 8211;
-            syncthingDiscovery = 21027;
-            syncthingTransfer = 22000;
-            palworldQuery = 27015;
-          };
+          default = { };
+          
         };
       };
     };
@@ -84,10 +45,54 @@ in
     description = "TCP and UDP listener ports used by services on lithium.";
   };
 
-  config.assertions = [
-    {
-      assertion = duplicates == [ ];
-      message = "Duplicate lithium service port assignments: ${lib.concatStringsSep "; " duplicates}";
-    }
-  ];
+  config = {
+    zw.servicePorts.tcp = {
+      adguardDns = 53;
+      caddyHttp = 80;
+      caddyHttps = 443;
+      sambaNetbiosSession = 139;
+      sambaSmb = 445;
+      forgejo = 3000;
+      grafana = 3001;
+      uptimeKuma = 3002;
+      immichMachineLearning = 3003;
+      adguardHome = 3004;
+      kanidmLdap = 3636;
+      sambaWsdd = 5357;
+      miniflux = 8081;
+      nextcloudBackend = 8083;
+      jellyfinHttp = 8096;
+      jellyfinHttps = 8920;
+      homeAssistant = 8123;
+      vaultwarden = 8222;
+      syncthingGui = 8384;
+      kanidmHttps = 8443;
+      calibreWeb = 8883;
+      hermesWebhook = 8644;
+      hermesDashboard = 9119;
+      hermesWebui = 18787;
+      syncthingTransfer = 22000;
+      palworldRcon = 25575;
+      immich = 2283;
+      paperless = 28981;
+    };
+    zw.servicePorts.udp = {
+      adguardDns = 53;
+      sambaNetbiosNs = 137;
+      sambaNetbiosDgm = 138;
+      sambaWsdd = 3702;
+      jellyfinSsdp = 1900;
+      jellyfinClientDiscovery = 7359;
+      palworldGame = 8211;
+      syncthingDiscovery = 21027;
+      syncthingTransfer = 22000;
+      palworldQuery = 27015;
+    };
+    assertions = [
+      {
+        assertion = duplicates == [ ];
+        message = "Duplicate lithium service port assignments: ${lib.concatStringsSep "; " duplicates}";
+      }
+    ];
+  };
 }
